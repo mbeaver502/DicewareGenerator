8eeeda0d97e5ff617518b30bfea1dd53553f26a3
Michael Beaver <jbeaver1@una.edu>
2015-03-27 13:13:06 -0500

    Initial commit
    All files.

---
+++ DicewareGenerator/frmMain.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace DicewareGenerator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.grpOptions = new System.Windows.Forms.GroupBox();
+            this.nudNumWords = new System.Windows.Forms.NumericUpDown();
+            this.grpCase = new System.Windows.Forms.GroupBox();
+            this.rbUpper = new System.Windows.Forms.RadioButton();
+            this.rbLower = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.cbDelimiters = new System.Windows.Forms.ComboBox();
+            this.lblDelimiter = new System.Windows.Forms.Label();
+            this.grpOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumWords)).BeginInit();
+            this.grpCase.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpOptions
+            // 
+            this.grpOptions.Controls.Add(this.lblDelimiter);
+            this.grpOptions.Controls.Add(this.cbDelimiters);
+            this.grpOptions.Controls.Add(this.label1);
+            this.grpOptions.Controls.Add(this.grpCase);
+            this.grpOptions.Controls.Add(this.nudNumWords);
+            this.grpOptions.Location = new System.Drawing.Point(12, 12);
+            this.grpOptions.Name = "grpOptions";
+            this.grpOptions.Size = new System.Drawing.Size(217, 108);
+            this.grpOptions.TabIndex = 1;
+            this.grpOptions.TabStop = false;
+            this.grpOptions.Text = " Options ";
+            // 
+            // nudNumWords
+            // 
+            this.nudNumWords.Location = new System.Drawing.Point(120, 32);
+            this.nudNumWords.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudNumWords.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumWords.Name = "nudNumWords";
+            this.nudNumWords.Size = new System.Drawing.Size(87, 20);
+            this.nudNumWords.TabIndex = 2;
+            this.nudNumWords.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.nudNumWords.ValueChanged += new System.EventHandler(this.nudNumWords_ValueChanged);
+            // 
+            // grpCase
+            // 
+            this.grpCase.Controls.Add(this.rbLower);
+            this.grpCase.Controls.Add(this.rbUpper);
+            this.grpCase.Location = new System.Drawing.Point(6, 19);
+            this.grpCase.Name = "grpCase";
+            this.grpCase.Size = new System.Drawing.Size(94, 83);
+            this.grpCase.TabIndex = 2;
+            this.grpCase.TabStop = false;
+            this.grpCase.Text = " Case ";
+            // 
+            // rbUpper
+            // 
+            this.rbUpper.AutoSize = true;
+            this.rbUpper.Checked = true;
+            this.rbUpper.Location = new System.Drawing.Point(6, 23);
+            this.rbUpper.Name = "rbUpper";
+            this.rbUpper.Size = new System.Drawing.Size(77, 17);
+            this.rbUpper.TabIndex = 3;
+            this.rbUpper.TabStop = true;
+            this.rbUpper.Text = "Uppercase";
+            this.rbUpper.UseVisualStyleBackColor = true;
+            this.rbUpper.CheckedChanged += new System.EventHandler(this.rbUpper_CheckedChanged);
+            // 
+            // rbLower
+            // 
+            this.rbLower.AutoSize = true;
+            this.rbLower.Location = new System.Drawing.Point(7, 52);
+            this.rbLower.Name = "rbLower";
+            this.rbLower.Size = new System.Drawing.Size(77, 17);
+            this.rbLower.TabIndex = 4;
+            this.rbLower.TabStop = true;
+            this.rbLower.Text = "Lowercase";
+            this.rbLower.UseVisualStyleBackColor = true;
+            this.rbLower.CheckedChanged += new System.EventHandler(this.rbLower_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of words:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(60, 126);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(258, 20);
+            this.txtOutput.TabIndex = 5;
+            this.txtOutput.WordWrap = false;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(235, 22);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(83, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(235, 51);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(83, 23);
+            this.btnAbout.TabIndex = 1;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(235, 80);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(83, 23);
+            this.btnQuit.TabIndex = 6;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // cbDelimiters
+            // 
+            this.cbDelimiters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDelimiters.FormattingEnabled = true;
+            this.cbDelimiters.Items.AddRange(new object[] {
+            "Space",
+            "_",
+            "~",
+            "/",
+            "\\",
+            "^"});
+            this.cbDelimiters.Location = new System.Drawing.Point(120, 77);
+            this.cbDelimiters.MaxLength = 1;
+            this.cbDelimiters.Name = "cbDelimiters";
+            this.cbDelimiters.Size = new System.Drawing.Size(87, 21);
+            this.cbDelimiters.TabIndex = 4;
+            this.cbDelimiters.SelectedIndexChanged += new System.EventHandler(this.cbDelimiters_SelectedIndexChanged);
+            // 
+            // lblDelimiter
+            // 
+            this.lblDelimiter.AutoSize = true;
+            this.lblDelimiter.Location = new System.Drawing.Point(117, 61);
+            this.lblDelimiter.Name = "lblDelimiter";
+            this.lblDelimiter.Size = new System.Drawing.Size(50, 13);
+            this.lblDelimiter.TabIndex = 5;
+            this.lblDelimiter.Text = "Delimiter:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 161);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.grpOptions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diceware Passphrase Generator v0.1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.grpOptions.ResumeLayout(false);
+            this.grpOptions.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumWords)).EndInit();
+            this.grpCase.ResumeLayout(false);
+            this.grpCase.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpOptions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpCase;
+        private System.Windows.Forms.RadioButton rbLower;
+        private System.Windows.Forms.RadioButton rbUpper;
+        private System.Windows.Forms.NumericUpDown nudNumWords;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.ComboBox cbDelimiters;
+        private System.Windows.Forms.Label lblDelimiter;
+    }
+}
+
---
+++ DicewareGenerator/frmMain.cs
@@ -0,0 +1,219 @@
+﻿// ----------------------------------------------------------------------------------
+// Diceware Passphrase Generator
+// ----------------------------------------------------------------------------------
+// Version: 0.1
+//  Author: J. Michael Beaver
+// ----------------------------------------------------------------------------------
+// This software is meant to be used to "randomly" generate passphrases based on the
+// Diceware system. This software was created in response to the following article:
+// https://firstlook.org/theintercept/2015/03/26/passphrases-can-memorize-attackers-cant-guess/.
+// Anyone may use and edit this software as permitted by the license below. This 
+// initial version is not particularly extensible due to much of the "variable" parts
+// being hardcoded (e.g., how many dice to roll). This will (likely) change with
+// future versions. Comments are sparse and functional decomposition is nonexistent.
+// Also, the wordlist file must be in the same directory as the executable. What you 
+// see is what you get.
+// ----------------------------------------------------------------------------------
+// Change Log:
+//      27 March 2015
+//          Initial version.
+// ----------------------------------------------------------------------------------
+// License: Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+// Wordlist: Arnold G. Reinold (http://world.std.com/~reinhold/) (CC BY 3.0 US)
+// Icon: FatCow (https://www.iconfinder.com/icons/64067/dice_icon) (CC BY 3.0 US)
+// ----------------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DicewareGenerator
+{
+    public partial class frmMain : Form
+    {
+
+        const int DICE_ROLLS = 5;
+        bool isUpperCase = true;
+        Dictionary<string, string> words = new Dictionary<string,string>();
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Diceware Passphrase Generator v0.1\n\n" + 
+                            "Author: J. Michael Beaver\n" + 
+                            "Wordlist: Arnold G. Reinhold\n" +
+                            "License: CC BY-NC-SA 4.0\n\n" + 
+                            "27 March 2015", 
+                            "About", 
+                            MessageBoxButtons.OK, 
+                            MessageBoxIcon.Information);
+        }
+
+
+        private void rbUpper_CheckedChanged(object sender, EventArgs e)
+        {
+            txtOutput.Text = txtOutput.Text.ToUpper();
+            isUpperCase = true;
+        }
+
+
+        private void rbLower_CheckedChanged(object sender, EventArgs e)
+        {
+            txtOutput.Text = txtOutput.Text.ToLower();
+            isUpperCase = false;
+        }
+
+
+        // Most of the important action happens here.
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+
+            Random r = new Random();
+            int num;
+            int numWords = (int)nudNumWords.Value;
+            string key = "";
+            string pWord = "";
+            string result = "";
+
+            // Build the passphrase.
+            for (int loopCount = 0; loopCount < numWords; loopCount++)
+            {
+                key = "";
+
+                // Build the 5-digit key that matches the password.
+                for (int i = 0; i < DICE_ROLLS; i++)
+                {
+                    num = r.Next(0, 6);
+                    num++;
+                    key += num.ToString();
+                }
+
+                // Get the password based on the key and add delimiters.
+                try
+                {
+                    pWord = words[key];
+
+                    if (isUpperCase)
+                        pWord = pWord.ToUpper();
+
+                    result += pWord;
+
+                    if (loopCount < numWords - 1)
+                    {
+                        if (cbDelimiters.SelectedItem.Equals("Space"))
+                            result += " ";
+
+                        else
+                            result += cbDelimiters.SelectedItem;
+                    }
+                }
+
+                catch (KeyNotFoundException)
+                {
+                    MessageBox.Show("Invalid key: " + key.ToString(), "Error!", 
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    loopCount--;
+                }
+            }
+                
+            txtOutput.Text = result;
+        }
+
+
+        // On the form load we will attempt to load the wordlist.
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cbDelimiters.SelectedIndex = 0;
+            
+            try
+            {
+                StreamReader file = new StreamReader("wordlist.txt");
+                string line;
+                string [] parts;
+
+                // Build the internal wordlist.
+                while ((line = file.ReadLine()) != null)
+                {
+                    parts = line.Split(new Char[] { ' ', '\t' });
+
+                    try
+                    {
+                        words.Add(parts[0], parts[1]);
+                    }
+                    
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("Duplicate key: " + line, "Error!",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+
+                file.Close();
+            }
+
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("\"wordlist.txt\" was not found!", "Error!", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnGo.Enabled = false;
+            }
+        }
+
+
+        // Here we can update the delimiters on the fly.
+        private void cbDelimiters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string find = "";
+            string replace = "";
+
+            if (txtOutput.Text == String.Empty)
+                return;
+
+            // (Badly) Determine which delimiter was being used previously.
+            foreach (string item in cbDelimiters.Items)
+            {
+                if (item == "Space")
+                    find = " ";
+                else
+                    find = item;
+
+                if (txtOutput.Text.IndexOf(find) >= 0)
+                    break;
+            }
+
+            // Update the delimiters.
+            replace = cbDelimiters.SelectedItem.ToString();
+            if (replace == "Space")
+                replace = " ";
+
+            txtOutput.Text = txtOutput.Text.Replace(find, replace);
+        }
+
+
+        private void nudNumWords_ValueChanged(object sender, EventArgs e)
+        {
+            btnGo_Click(sender, e);
+        }
+    }
+}
